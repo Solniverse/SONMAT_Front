c79f1647c7393c159811d31b5527b951bf5513a2
chosohi <chosohi@naver.com>
2023-06-18 16:48:39 +0900

    feat: main page semantic tage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from 'next/image';
+import Footer from '@/components/footer';
 
 export default function Home() {
   return (
-    <main className=" to flex min-h-screen flex-col items-center justify-between bg-gradient-to-b from-indigo-900 from-60% via-white via-80% to-pink-500 to-100% p-24 ">
-      <div className="">test</div>
-    </main>
+    <div className="flex min-h-screen flex-col items-center justify-between  bg-gradient-to-b from-darkPurlple from-70% to-purple to-100% p-24">
+      <header className="">Let&apos;s FingerGame</header>
+      <main></main>
+      <Footer />
+    </div>
   );
 }
