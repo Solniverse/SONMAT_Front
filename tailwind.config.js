3c94e23fe1f58d658a65e7ce0bdb6d1db07e1b49
chosohi <chosohi@naver.com>
2023-06-18 16:27:21 +0900

    style: theme color test

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,9 @@ module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        darkPurlple: '#1A1C48',
+        purple: '#9499C3',
       },
     },
   },
